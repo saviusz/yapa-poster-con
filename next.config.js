05ed54ccc540e8a8b90a3cacc70e158a6d450139
Seweryn Woliński <serweryn123@gmail.com>
2025-02-03 00:22:07 +0100

    feat: allow bigger images
    Signed-off-by: Seweryn Woliński <serweryn123@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
     sassOptions: {
         silenceDeprecations: ['legacy-js-api']
+    },
+    experimental: {
+        serverActions: {
+            bodySizeLimit: '50mb'
+        }
     }
 };
 
